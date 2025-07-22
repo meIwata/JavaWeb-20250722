fd255810aa32d1034e1e17924efcab17a771f647
meIwata <waleilulu@gmail.com>
2025-07-22 20:35:37 +0800

    update

---
+++ src/main/java/dao/BaseDao.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// 負責管理資料庫連線
+public class BaseDao {
+	
+	private static Connection conn;
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&useUnicode=true&characterEncoding=utf8&useSSL=false";
+			String username = "root";
+			String password = "12345678";
+			conn = DriverManager.getConnection(dbUrl, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected Connection getConnection() {
+		return conn;
+	}
+	
+}
+
+// 如果要再連其他資料庫，可以再建一個public class XXX 連到另一個資料庫
---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+import model.User;
+
+public interface UserDao {
+
+	List<User> findAllUsers();
+	User getUserById(Integer id);
+	User getUserByUsername(String name);
+	void addUser(String name, String hash, String salt);
+	void changePasswordById(Integer id, String newHash, String newSalt);
+	void deleteUserById(Integer id);
+}
