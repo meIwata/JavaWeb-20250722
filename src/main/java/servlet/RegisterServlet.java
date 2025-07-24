cd604ed14fe2ce4f5c17236eb0e96094371ba278
meIwata <waleilulu@gmail.com>
2025-07-24 19:13:40 +0800

    RegisterServlet-doGet

--- src/main/java/servlet/RegisterServlet.java
+++ src/main/java/servlet/RegisterServlet.java
@@ -19,7 +19,6 @@ public class RegisterServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		
 	}
 }
